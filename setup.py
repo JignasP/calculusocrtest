458d56038ab1a0677978f527d5052f08f83b85dd
Jignas Paturu <86356085+JignasP@users.noreply.github.com>
2023-09-03 19:16:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,17 +11,17 @@ cocr_remote_version = (
     .strip()
 )
 
-#if "-" in cocr_remote_version:
+if "-" in cocr_remote_version:
 
-#    v,i,s = cocr_remote_version.split("-")
-#    cocr_remote_version = v + "+" + i + ".git." + s
+    v,i,s = cocr_remote_version.split("-")
+    cocr_remote_version = v + "+" + i + ".git." + s
 
-#assert "-" not in cocr_remote_version
-#assert "." in cocr_remote_version
+assert "-" not in cocr_remote_version
+assert "." in cocr_remote_version
 
-#assert os.path.isfile("CalculusOCR/version.py")
-#with open("CalculusOCR/VERSION", "w", encoding="utf-8") as fh:
- #   fh.write("%s\n" % cocr_remote_version)
+assert os.path.isfile("CalculusOCR/version.py")
+with open("CalculusOCR/VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % cocr_remote_version)
 
 
 this_directory = Path(__file__).parent
@@ -31,7 +31,7 @@ long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='CalculusOCR',
-    version='cocr_remote_version',
+    version=cocr_remote_version,
     description='CalculusOCR: Using a ViT to solve images of equations .',
     long_description=long_description,
     long_description_content_type='text/markdown',
